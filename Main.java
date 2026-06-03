0fe1c56303ab9c9b029bc8f97873389ebeef080b
Manuem-dev <yemoletrader@gmail.com>
2026-06-03 22:54:51 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,15 +6,15 @@ public class Main {
 	public static void main(String[] args) {
 
 		// Variables
-		
+		String statut = new String();
 
 		Scanner input = new Scanner(System.in);
 
 		System.out.println("Se connecter en tant que:");
-		System.out.println("G : Gerant");
-		System.out.println("R : Réceptionniste");
-		System.out.println("V : Vendeur");
-
+		System.out.println("G/g : Gerant");
+		System.out.println("R/r : Réceptionniste");
+		System.out.println("V/v : Vendeur");
+		statut = input.next();
 		
 
 		switch (i){
