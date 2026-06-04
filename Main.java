ea58be1f14cd07b4f6e720b1fe4d4bf33d3989a4
ysy <yemoletrader@gmail.com>
2026-06-04 00:59:49 +0000

    Code logique complet

--- Main.java
+++ Main.java
@@ -36,7 +36,15 @@ public class Main {
 
 				switch (role.toLowerCase()) {
 					case "a":
-						
+						String nom = new String();
+						double price;
+						System.out.print("Veuillez entrer le nom du produit à ajouter: ");
+						nom = input.next();
+						System.out.print("Veuillez le prix de ce produit: ");
+						price = input.nextDouble();
+						produits.add(new Produits(nom, price));
+						System.out.println("Le produit " + nom + " a été enrégistré avec succès au prix de " + price);
+
 						break;
 					case "e":
 						System.out.println("Voici la liste des produits disponibles; veuillez choisir un produit par son indice");
@@ -65,10 +73,25 @@ public class Main {
 				
 						break;
 					case "p":
+						int total=0;
+						for(int i=0;i<produits.size();i++){
+							total = total + produits.get(i).getStock();
+						}
+						if (total > 0) {
+							System.out.println("Le magasin comporte: ");
+							for(int i=0;i<produits.size();i++){
+								produits.get(i).toString();
+							}
+						} else{
+							System.out.println("Le magasin est vide pour le moment, veuillez donc ajoutez des produits ");
+						}
 						
 						break;
 					case "r":
-						
+						System.out.println("Les produits en rupture de stock sont: ");
+						for(int i=0;i<produits.size();i++){
+							produits.get(i).ruptureStock();;
+						}
 						break;
 				
 					
@@ -76,31 +99,42 @@ public class Main {
 				
 				break;
 			case "r":
-				System.out.println("Bienvenue Réceptionniste");
-				System.out.println("Voici la liste des produits disponibles; veuillez choisir un produit par son indice");
-				for(int i=0;i<produits.size();i++){
-					System.out.println("[indice: "+ (i+1) + " | produit: " + produits.get(i).getNom() + "]");
+				if (produits.size()>0) {
+					
+					System.out.println("Bienvenue Réceptionniste");
+					System.out.println("Voici la liste des produits disponibles; veuillez choisir un produit par son indice");
+					for(int i=0;i<produits.size();i++){
+						System.out.println("[indice: "+ (i+1) + " | produit: " + produits.get(i).getNom() + "]");
+					}
+					System.out.print("Votre choix: ");
+					choix = input.nextInt();
+					System.out.print("Vous avez choisi le produit [" + produits.get(choix-1).getNom() + "];\nveuillez entrer la quantité que vous souhaiter entreposer: "   );
+					newQuantite = input.nextInt();
+					produits.get(choix-1).entree(newQuantite);
+					System.out.println("Vous avez ajouté " + newQuantite + " produit(s) à l'entrepôt de " + produits.get(choix-1).getNom());
+				} else{
+					System.out.println("Le magasin est vide pour le moment, laissez le gérant ajouter un produit d'abord");
 				}
-				System.out.print("Votre choix: ");
-				choix = input.nextInt();
-				System.out.print("Vous avez choisi le produit [" + produits.get(choix-1).getNom() + "];\nveuillez entrer la quantité que vous souhaiter entreposer: "   );
-				newQuantite = input.nextInt();
-				produits.get(choix-1).entree(newQuantite);
-				System.out.println("Vous avez ajouté " + newQuantite + " produit(s) à l'entrepôt de " + produits.get(choix-1).getNom());
 				
 				break;
 			case "v":
 				System.out.println("Bienvenue Vendeur");
-				System.out.println("Voici la liste des produits disponibles; veuillez choisir un produit par son indice");
-				for(int i=0;i<produits.size();i++){
-					System.out.println("[indice: "+ (i+1) + " | produit: " + produits.get(i).getNom() + "]");
+				if (produits.size()>0) {
+					
+					System.out.println("Voici la liste des produits disponibles; veuillez choisir un produit par son indice");
+					for(int i=0;i<produits.size();i++){
+						System.out.println("[indice: "+ (i+1) + " | produit: " + produits.get(i).getNom() + "]");
+					}
+					System.out.print("Votre choix: ");
+					choix = input.nextInt();
+					System.out.print("Vous avez choisi le produit [" + produits.get(choix-1).getNom() + "]; \n veuillez entrer la quantité que vous souhaiter vendre                  : "   );
+					newQuantite = input.nextInt();
+					produits.get(choix-1).entree(newQuantite);
+					System.out.println("Vous avez retiré " + newQuantite + " produit(s) à l'entrepôt de " + produits.get(choix-1).getNom());
+				} else{
+					
+					System.out.println("Le magasin est vide pour le moment, laissez le gérant ajouter un produit d'abord");
 				}
-				System.out.print("Votre choix: ");
-				choix = input.nextInt();
-				System.out.print("Vous avez choisi le produit [" + produits.get(choix-1).getNom() + "]; \n veuillez entrer la quantité que vous souhaiter vendre                  : "   );
-				newQuantite = input.nextInt();
-				produits.get(choix-1).entree(newQuantite);
-				System.out.println("Vous avez retiré " + newQuantite + " produit(s) à l'entrepôt de " + produits.get(choix-1).getNom());
 				
 				
 				break;
